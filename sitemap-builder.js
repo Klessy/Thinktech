0346312077d82c97b548f9d0ec26aa0c6c8da36f
Precious Eruteya <eruteyaprecious@gmail.com>
2025-05-16 20:34:45 +0100

    package react-router-sitemap-removed

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,11 +1,11 @@
 // sitemap-builder.js
-const Sitemap = require("react-router-sitemap").default;
-const path = require("path");
+// const Sitemap = require("react-router-sitemap").default;
+// const path = require("path");
 
-function generateSitemap() {
-  return new Sitemap(require("./src/App").default)
-    .build("https://yourdomain.com") // Replace with your domain
-    .save(path.resolve(__dirname, "public", "sitemap.xml"));
-}
+// function generateSitemap() {
+//   return new Sitemap(require("./src/App").default)
+//     .build("https://yourdomain.com") // Replace with your domain
+//     .save(path.resolve(__dirname, "public", "sitemap.xml"));
+// }
 
-generateSitemap();
+// generateSitemap();
